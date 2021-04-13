e94e12cc8c42299a5536ff696e10560b911379f1
FabrizioRozzi <rozzi458@gmail.com>
2021-04-13 19:57:52 +0200

    finito

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+      // PRIMO ESERCIZIO
+
+  //PRIMA VERSIONE CON FOR
+  
+  //Dichiarazione e inizializzazione variabili
+// var totale = 0;
+  
+  //Eseguo ciclo for per 5 volte
+// for(var i = 0; i < 5; i++ ){
+//   var n = parseInt(prompt("Inserisci un numero..."));
+//   totale = totale + n;
+// }
+    
+  //Risultato
+// console.log("il risultato é: " + totale);
+
+
+  //SECONDA VERSIONE CON WHILE
+
+  //Dichiarazione e inizializzazione variabili
+// var totale = 0;
+// var i = 0;
+
+//   //Eseguo ciclo while
+// while(i < 5){
+//   var n = parseInt(prompt("Inserisci un numero..."));
+//   totale = totale + n;
+//   i++;
+// }  
+
+// //Risultato
+// console.log("il risultato é: " + totale);
+
+
+
+        //SECONDO ESERCIZIO
+
+  //Dichiarazione e inizializzazione variabili
+var totale = 0;    
+var i = 0;
+
+while(i < 1){
+  var n = parseInt(prompt("Inserisci un numero..."));
+
+  if( n % 2 == 0 ){
+    console.log(n);
+  }
+}     
